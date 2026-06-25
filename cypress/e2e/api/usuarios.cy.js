5d1e5d0fbc9a51cf3bafa5825e49facd6083e1bd
Marcia MagaX <135170338+MarciaMagaX@users.noreply.github.com>
2026-06-24 21:24:17 -0300

    feat: adicionado cenário de cadastro de usuários com fixture

--- cypress/e2e/api/usuarios.cy.js
+++ cypress/e2e/api/usuarios.cy.js
@@ -20,3 +20,62 @@ describe('API ServeRest - Testes na rota de Usuários', () => {
   });
 
 });
+
+// describe('API ServeRest - Testes na rota de Usuários', () => {
+
+//   it('Deve listar os usuários cadastrados com sucesso', () => {
+//     // ... nosso teste GET anterior continua aqui ...
+//   });
+
+//   // NOSSO NOVO TESTE POST
+//   it('Deve cadastrar um novo usuário com sucesso', () => {
+    
+//     cy.request({
+//       method: 'POST',
+//       url: '/usuarios',
+//       body: {
+//         "nome": "QA Junior Teste",
+//         "email": "qajunior123@qa.com.br",
+//         "password": "teste",
+//         "administrador": "true"
+//       }
+//     }).then((response) => {
+//       // Validações
+//       expect(response.status).to.eq(201); // 201 significa "Created" (Criado com sucesso)
+//       expect(response.body.message).to.eq('Cadastro realizado com sucesso');
+      
+//       // A API devolve o ID do usuário criado, podemos garantir que ele existe
+//       expect(response.body).to.have.property('_id'); 
+//     });
+
+//   });
+
+// });
+
+describe('API ServeRest - Testes na rota de Usuários', () => {
+
+  it('Deve cadastrar um novo usuário com sucesso usando fixture', () => {
+    
+    // Gerador de e-mail dinâmico usando o milissegundo atual
+    const emailDinamico = `qa_${Date.now()}@qa.com.br`;
+
+    // cy.fixture() vai buscar o arquivo na pasta cypress/fixtures/
+    cy.fixture('usuario').then((massaDeDados) => {
+      
+      // Adicionamos o e-mail dinâmico à nossa massa de dados
+      massaDeDados.email = emailDinamico;
+
+      cy.request({
+        method: 'POST',
+        url: '/usuarios',
+        body: massaDeDados // Injetamos o JSON limpo e dinâmico aqui
+      }).then((response) => {
+        expect(response.status).to.eq(201);
+        expect(response.body.message).to.eq('Cadastro realizado com sucesso');
+      });
+
+    });
+
+  });
+
+});
